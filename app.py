ff2ee63f9efe798289537d0af91e6d03e18cdcab
ibraloyza <ibraahim3523@gmai=l.com>
2025-09-28 21:10:40 +0300

    added 4 exerciese

--- app.py
+++ app.py
@@ -40,24 +40,46 @@
 #  controll flow
 
 
-grade = int(input("Enter your score : "));
+# grade = int(input("Enter your score : "));
 
-if grade >= 90:
-    print( "your grade is A+ ")
-elif grade >= 85 and grade <= 89:
-    print("your grade is A ");
-elif grade >= 80 and grade <= 84:
-    print("your grade is A-");
-elif grade >= 75 and grade <= 89:
-    print("you grade is B");
-elif grade >= 70 and grade <= 74:
-    print("your grade is B-");
-elif grade >= 65 and  grade <= 70:
-    print("your grade  is  C");
-elif grade >= 60 and grade <= 64:
-    print("your grade is C-");
-else:
-    print("you grade is F and you ar a  lossser");
+# if grade >= 90:
+#     print( "your grade is A+ ")
+# elif grade >= 85 and grade <= 89:
+#     print("your grade is A ");
+# elif grade >= 80 and grade <= 84:
+#     print("your grade is A-");
+# elif grade >= 75 and grade <= 89:
+#     print("you grade is B");
+# elif grade >= 70 and grade <= 74:
+#     print("your grade is B-");
+# elif grade >= 65 and  grade <= 70:
+#     print("your grade  is  C");
+# elif grade >= 60 and grade <= 64:
+#     print("your grade is C-");
+# else:
+#     print("you grade is F and you ar a  lossser");
 
 
+# 1 . write if and alse  program that checks is old enough age 
+# age = int(input("Enter your age: "));
+
+# if age >= 18:
+#     print("you age is  an eligibale to vote");
+# else:
+#     print("you are a too yange");
+
+# for number in range(10):
+#     print(number)
+
+# count = 10;
+
+# while count >= 1:
+#     print(count)
+#     count -=1;
+
+
+gobolada = ["banaadir","baydhabo","bay","bakool"];
+
+for g in gobolada:
+    print(g);
 
