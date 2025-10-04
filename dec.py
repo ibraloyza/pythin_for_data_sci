1f784a4bf0d9e17a2b36ea7c27c5f535e5b17d4f
ibraloyza <ibraahim3523@gmai=l.com>
2025-10-04 19:07:49 +0300

    adde exercise � Beginner Level (Basics of Dictionary)

--- dec.py
+++ dec.py
@@ -33,3 +33,35 @@ for key , value in my_info.items():
 square = {x: x**2 for x in range(10)}
 print(f"the square of numbers from 0 to 9 is : {square}");
 
+#  exercise 🟢 Beginner Level (Basics of Dictionary)
+# Create a dictionary called student with keys: "name", "age", and "grade".Assign any values you want.
+
+student = {
+    "name ": "ibrahim",
+    "age": 25,
+    "grade": "A"
+}
+
+# Print only the "name" from the dictionary.
+print(f"the name of  the student is : {student['name ']}")
+
+# Add a new key "country" with any value you want.
+student["country"] = "somalia"
+print(f"the updated student dictionary is : {student}")
+
+# Change the student’s "grade" to "A+".
+student["grade"] = "A+"
+print(f"the updated grade of the student is : {student}")
+
+# Delete the "age" key.
+del student["age"];
+print(student)
+
+# Loop through all the keys and values, printing them like: name: Ali, grade: A+, etc
+for key, value in student.items():
+    print(key+" : "+value);
+
+# Use .get() to safely get a key that doesn’t exist (like "email") and print what it returns.
+print(student.get('email', "Email not found"));
+
+
